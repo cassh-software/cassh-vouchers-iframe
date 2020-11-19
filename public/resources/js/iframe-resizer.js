1b9ad0f125d322462dc952e58ff8b3bce136b87d
Josef van Niekerk <safrique@users.noreply.github.com>
2020-11-19 23:27:17 +0000

    CAS-54 => Moved hosting site scripts to js cloud

--- public/resources/js/iframe-resizer.js
+++ public/resources/js/iframe-resizer.js
@@ -1,50 +1,50 @@
 window.onload = () => {
-  console.log(`Loading js file...`)
+  // console.log(`Loading js file...`)
   let head = window.document.getElementsByTagName(`head`)[0]
-  console.log(`head --->>>`, head)
+  // console.log(`head --->>>`, head)
 
   let jquery = window.document.createElement(`script`)
   jquery.setAttribute(`src`, `http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js`)
   // jquery.setAttribute(`defer`, `defer`)
-  console.log(`jquery --->>>`, jquery)
+  // console.log(`jquery --->>>`, jquery)
   head.appendChild(jquery)
 
   let resizer = window.document.createElement(`script`)
   resizer.setAttribute(`type`, `text/javascript`)
   resizer.setAttribute(`src`, `https://cassh-software.github.io/cassh-vouchers-iframe/public/resources/js/iframeResizer.min.js`)
   // resizer.setAttribute(`defer`, `defer`)
-  console.log(`resizer --->>>`, resizer)
+  // console.log(`resizer --->>>`, resizer)
   head.appendChild(resizer)
 
-  let function_call = function () {
-    console.log(`Running iFrameResize...`)
-    iFrameResize({
-      log: false,
-      enablePublicMethods: true,
-      resizedCallback: function (messageData) {
-        $('p#callback').html(
-          '<b>Frame ID:</b> ' + messageData.iframe.id +
-          ' <b>Height:</b> ' + messageData.height +
-          ' <b>Width:</b> ' + messageData.width +
-          ' <b>Event type:</b> ' + messageData.type
-        )
-      },
-      messageCallback: function (messageData) {
-        $('p#callback').html(
-          '<b>Frame ID:</b> ' + messageData.iframe.id +
-          ' <b>Message:</b> ' + messageData.message
-        )
-        alert(messageData.message)
-      },
-      closedCallback: function (id) {
-        $('p#callback').html(
-          '<b>IFrame (</b>' + id +
-          '<b>) removed from page.</b>'
-        )
-      }
-    })
-  }
-  console.log(`function_call --->>>`, function_call)
+  // let function_call = function () {
+  //   console.log(`Running iFrameResize...`)
+  //   iFrameResize({
+  //     log: false,
+  //     enablePublicMethods: true,
+  //     resizedCallback: function (messageData) {
+  //       $('p#callback').html(
+  //         '<b>Frame ID:</b> ' + messageData.iframe.id +
+  //         ' <b>Height:</b> ' + messageData.height +
+  //         ' <b>Width:</b> ' + messageData.width +
+  //         ' <b>Event type:</b> ' + messageData.type
+  //       )
+  //     },
+  //     messageCallback: function (messageData) {
+  //       $('p#callback').html(
+  //         '<b>Frame ID:</b> ' + messageData.iframe.id +
+  //         ' <b>Message:</b> ' + messageData.message
+  //       )
+  //       alert(messageData.message)
+  //     },
+  //     closedCallback: function (id) {
+  //       $('p#callback').html(
+  //         '<b>IFrame (</b>' + id +
+  //         '<b>) removed from page.</b>'
+  //       )
+  //     }
+  //   })
+  // }
+  // console.log(`function_call --->>>`, function_call)
 
   let caller = `    console.log(\`Running iFrameResize...\`)
     iFrameResize({
@@ -79,9 +79,9 @@ window.onload = () => {
   // resize.setAttribute(`defer`, `defer`)
   let inline = window.document.createTextNode(caller)
   resize.appendChild(inline)
-  console.log(`resize --->>>`, resize)
+  // console.log(`resize --->>>`, resize)
   head.appendChild(resize)
   // function_call()
 
-  console.log(`Done...`)
+  // console.log(`Done...`)
 }
